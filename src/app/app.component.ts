5820df6bb279440bb8e65d3d8748ad6ad1b42e17
SkibidiMeier <sandro.meier@buhlergroup.com>
2025-06-19 14:26:34 +0200

    Commit WWW

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
-import { SplashScreenComponent } from './components/splash-screen/splash-screen.component';
 
 @Component({
   standalone: true,
   selector: 'app-root',
   templateUrl: './app.component.html',
-  imports: [IonApp, IonRouterOutlet, SplashScreenComponent, CommonModule],
+  imports: [IonApp, IonRouterOutlet, CommonModule],
 })
 export class AppComponent {
   showSplash = true;
